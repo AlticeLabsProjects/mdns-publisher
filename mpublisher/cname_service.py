89a1aecb660b6110975e04800a66eabc4633600e
Carlos Rodrigues <cer@brpx.com>
2018-04-08 15:41:24 +0100

    Switch to argparse

--- mpublisher/cname_service.py
+++ mpublisher/cname_service.py
@@ -20,8 +20,7 @@ import re
 import signal
 import functools
 
-from getopt import getopt, GetoptError
-from textwrap import TextWrapper
+from argparse import ArgumentParser, ArgumentTypeError
 from time import sleep
 
 from .mpublisher import AvahiPublisher
@@ -34,78 +33,49 @@ log = logging.getLogger("mdns-publisher")
 DEFAULT_DNS_TTL = 60
 
 
-def print_usage():
-    """Output the proper usage syntax for this program."""
 
-    print("USAGE: %s [-t <ttl>] [-f] [-v] <hostname.local> [...]" % os.path.basename(sys.argv[0]))
+def positive_int_arg(value):
+    """Helper type (for argparse) to validate and return positive integer argument."""
 
-    wrapper = TextWrapper(width=79, initial_indent="\t", subsequent_indent="\t")
+    try:
+        ivalue = int(value)
+    except ValueError:
+        raise ArgumentTypeError("invalid int value: %s" % repr(value))
 
-    print("\n-t/--ttl <seconds>")
-    print(wrapper.fill("Set the TTL for all published records. (Default: %ds)" % DEFAULT_DNS_TTL))
+    if ivalue <= 0:
+        raise ArgumentTypeError("value must be greater than zero")
 
-    print("\n-f/--force")
-    print(wrapper.fill("Publish all CNAMEs without checking if they are already being published "
-                       "elsewhere on the network. This is much faster, but generally unsafe."))
+    return ivalue
 
-    print("\n-v/--verbose")
-    print(wrapper.fill("Produce extra output for debugging purposes."))
 
-    print("\n-d/--daemon")
-    print(wrapper.fill("Run the CNAME publishing service in the background."))
+def local_hostname_arg(hostname):
+    """Helper type (for argparse) to validate and return a (normalized) local hostname argument."""
 
-    print("\n-l/--log=<filename>")
-    print(wrapper.fill("Send log messages into the specified file."))
+    if not re.match(r"^[a-z0-9][a-z0-9_-]*(?:\.[a-z0-9][a-z0-9_-]*)*\.local$", hostname, re.I):
+        raise ArgumentTypeError("malformed CNAME: %s" % repr(hostname))
+
+    return hostname.lower()
 
 
 def parse_args():
     """Parse and enforce command-line arguments."""
 
-    try:
-        options, args = getopt(sys.argv[1:], "t:fvdl:h", ["ttl=", "force", "verbose",
-                                                          "daemon", "log=", "help"])
-    except GetoptError as e:
-        print("error: %s." % e, file=sys.stderr)
-        print_usage()
-        sys.exit(1)
+    # Disable the automatic "-h/--help" argument to customize its message...
+    parser = ArgumentParser(description="Publish CNAMEs pointing to the local host over Avahi/mDNS.", add_help=False)
 
-    if len(args) < 1:
-        print("error: parameter(s) missing.", file=sys.stderr)
-        print_usage()
-        sys.exit(1)
+    parser.add_argument("cnames", metavar="hostname", nargs="+", type=local_hostname_arg,
+                                  help="Fully-qualified CNAME(s) to publish. Subdomains are "
+                                       "allowed, but names must end with the '.local' domain.")
 
-    # Minimal checking that the CNAMEs are properly formatted...
-    cname_re = re.compile(r"^[a-z0-9-]{1,63}(?:\.[a-z0-9-]{1,63})*\.local$")
-    cnames = [arg.strip().lower() for arg in args]
+    parser.add_argument("-h", "--help", action="help", help="Show the available options and exit.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Produce extra output for debugging purposes.")
+    parser.add_argument("-d", "--daemon", action="store_true", help="Run the publishing service in the background.")
+    parser.add_argument("-f", "--force", action="store_true", help="Do not check for availability before publishing.")
+    parser.add_argument("-l", "--log", action="store", metavar="file", help="Log messages into the specified file.")
+    parser.add_argument("-t", "--ttl", action="store", metavar="secs", type=positive_int_arg, default=DEFAULT_DNS_TTL,
+                                       help="TTL for published records, in seconds. (Default: %ds)" % DEFAULT_DNS_TTL)
 
-    for cname in cnames:
-        if not cname_re.match(cname):
-            print("error: malformed hostname: %s" % cname, file=sys.stderr)
-            print_usage()
-            sys.exit(1)
-
-    ttl = DEFAULT_DNS_TTL
-    force = False
-    verbose = False
-    daemon = False
-    logname = None
-
-    for option, value in options:
-        if option in ("-h", "--help"):
-            print_usage()
-            sys.exit(1)
-        elif option in ("-t", "--ttl"):
-            ttl = int(value)
-        elif option in ("-f", "--force"):
-            force = True
-        elif option in ("-v", "--verbose"):
-            verbose = True
-        elif option in ("-d", "--daemon"):
-            daemon = True
-        elif option in ("-l", "--log"):
-            logname = value.strip()
-
-    return (ttl, force, verbose, daemon, logname, cnames)
+    return parser.parse_args()
 
 
 def daemonize():
@@ -168,26 +138,26 @@ def handle_signals(publisher, signum, frame):
 
 
 def main():
-    (ttl, force, verbose, daemon, logfile, cnames) = parse_args()
+    args = parse_args()
 
     # Since an eventual log file must support external log rotation, we must do this the hard way...
     format = logging.Formatter("%(asctime)s: %(levelname)s [%(process)d]: %(message)s")
-    handler = logging.handlers.WatchedFileHandler(logfile) if logfile else logging.StreamHandler(sys.stderr)
+    handler = logging.handlers.WatchedFileHandler(args.log) if args.log else logging.StreamHandler(sys.stderr)
     handler.setFormatter(format)
     logging.getLogger().addHandler(handler)
 
     # Leaving the root logger with the default level, and setting it in our own logging hierarchy
     # instead, prevents accidental triggering of third-party logging, just like $DEITY intended...
-    log.setLevel(logging.DEBUG if verbose else logging.INFO)
+    log.setLevel(logging.DEBUG if args.verbose else logging.INFO)
 
     # This must be done after initializing the logger, so that an eventual log file gets created in
     # the right place (the user will assume that relative paths start from the current directory)...
-    if daemon:
+    if args.daemon:
         daemonize()
 
     log.info("Avahi/mDNS publisher starting...")
 
-    if force:
+    if args.force:
         log.info("Forcing CNAME publishing without collision checks")
 
     # The publisher needs to be initialized in the loop, to handle disconnects...
@@ -195,23 +165,23 @@ def main():
 
     while True:
         if not publisher or not publisher.available():
-            publisher = AvahiPublisher(ttl)
+            publisher = AvahiPublisher(args.ttl)
 
             # To make sure records disappear immediately on exit, clean up properly...
             signal.signal(signal.SIGTERM, functools.partial(handle_signals, publisher))
             signal.signal(signal.SIGINT, functools.partial(handle_signals, publisher))
             signal.signal(signal.SIGQUIT, functools.partial(handle_signals, publisher))
 
-            for cname in cnames:
-                status = publisher.publish_cname(cname, force)
+            for cname in args.cnames:
+                status = publisher.publish_cname(cname, args.force)
                 if not status:
                     log.error("Failed to publish '%s'", cname)
                     continue
 
-            if publisher.count() == len(cnames):
+            if publisher.count() == len(args.cnames):
                 log.info("All CNAMEs published")
             else:
-                log.warning("%d out of %d CNAMEs published", publisher.count(), len(cnames))
+                log.warning("%d out of %d CNAMEs published", publisher.count(), len(args.cnames))
 
         # CNAMEs will exist while this service is kept alive,
         # but we don't actually need to do anything useful...
